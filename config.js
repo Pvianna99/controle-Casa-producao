297f224927f36020311ed1a08bb9d1bb321501b0
Pvianna99 <Pedro.pockrandt@gmail.com>
2026-09-23 18:04:26 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ window.APP_CONFIG = {
   SUPABASE_URL: "https://hdrkojbdczjaqzzufvno.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcmtvamJkY3pqYXF6enVmdm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxMTkzNjQsImV4cCI6MjEwNTY5NTM2NH0.o5DjyPVBvyJb49k93MTpq3QKtWhHM4lp4ScEPPnFCqw",
   NOME_APP: "Controle de Produção",   // nome que aparece no topo do app
+  EXIGIR_LOGIN: false,                 // true = pede e-mail e senha (rode também o com-login.sql)
 };
